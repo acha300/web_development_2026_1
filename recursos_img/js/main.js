801c4b14881df2d3d20eaf650e81161842b1d4cc
acha300 <elianherrera53@gmail.com>
2026-02-16 11:58:18 -0500

    d

--- recursos_img/js/main.js
+++ recursos_img/js/main.js
@@ -1,6 +1,17 @@
 const x = document.getElementById("result");
+
 if (typeof(Storage) !== "undefined") {
   x.innerHTML = "Your browser supports Web storage!";
 } else {
   x.innerHTML = "Sorry, no Web storage support!";
+}
+
+function saveToLocalStorage() {
+    var name = document.getElementById("nameLocal").value;
+    alert(name);
+}
+
+function saveToSessionStorage() {
+    var name = document.getElementById("nameSession").value;
+    alert(name);
 }
