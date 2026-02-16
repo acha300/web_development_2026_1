ad8d3e2c86f649db8adfe5de8d74b8a2bafcf3ea
acha300 <elianherrera53@gmail.com>
2026-02-16 11:30:14 -0500

    cs

--- recursos_img/js/main.js
+++ recursos_img/js/main.js
@@ -1,1 +1,6 @@
-alert("hola mundo");
+const x = document.getElementById("result");
+if (typeof(Storage) !== "undefined") {
+  x.innerHTML = "Your browser supports Web storage!";
+} else {
+  x.innerHTML = "Sorry, no Web storage support!";
+}
